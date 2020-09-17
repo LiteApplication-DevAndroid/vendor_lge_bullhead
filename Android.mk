cb185e809691d205e00f4a31fc6712d959a83be3
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-09-17 18:26:01 +0200

    bullhead: remove libtime_genoff

--- Android.mk
+++ Android.mk
@@ -269,17 +269,17 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libtime_genoff
-LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libtime_genoff.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libtime_genoff.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libtime_genoff
+#LOCAL_MODULE_OWNER := lge
+#LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libtime_genoff.so
+#LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libtime_genoff.so
+#LOCAL_MULTILIB := both
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_VENDOR_MODULE := true
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := datastatusnotification
